3970bec901d17dd3583f87b541db68c4faac3353
isaacmichael <54340385+isaacmichael@users.noreply.github.com>
2022-04-29 13:56:41 -0400

    FIRST COMMIT

--- src/App.js
+++ src/App.js
@@ -3,22 +3,33 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <body>
+      <div>
+          <h1 class="head">Welcome To Isaac Pearson's Website</h1>
+      </div>
+      <div class="grid">
+          <p class = "p" id="p1">Item1</p>
+          <p class="p" id="p2">Item2</p>
+          <p class="p" id="p3">Item3</p>
+      </div>
+      <div class = "section1">
+          <h2>DETAILED SUMMARY</h2>
+          <p>
+              <br></br>
+              sldmfhnldksjfhlkjdshfjkl<br></br>
+              sdkjhfgbjdkshfgdkjhsfgkjh
+              skjnhgbdfvbjkhdsgfjkhdsgfkjhdsgkjhfgdshjkfgjhkdsfgkjhdsfgkjhfgdehukjg<br></br>
+              dl;ksfhjlkjdshfkljsdhkjdshklfhjklsdfh<br></br>
+              dskjfhlkjdshflkdshlfkjhdsjklfhds<br></br>
+              dskjfhlkjdshflkdshlfkjhdsjklfhds<br></br>
+          </p>
+      </div>
+
+      <footer>
+          footer
+      </footer>
+
+      </body>
   );
 }
 
