ec81257e259eda053a33b0170b721597ff11c6f5
isaacmichael <54340385+isaacmichael@users.noreply.github.com>
2022-04-30 17:56:02 -0400

    basic additions

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import './App.css';
 function App() {
   return (
       <body>
-      <div>
-          <h1 class="head">Welcome To Isaac Pearson's Website</h1>
+      <div class="head">
+          <h1 id="home">Isaac Pearson</h1>
+          <h3>SOFTWARE DEVELOPER</h3>
+          <a class="nav">Sections</a>
+          <a href="#home" class="linkOne">1</a>
+          <a href="#summary" class="linkTwo">2</a>
+          <a href="#section3" class="linkThree">3</a>
       </div>
       <div class="grid">
-          <p class = "p" id="p1">Item1</p>
-          <p class="p" id="p2">Item2</p>
-          <p class="p" id="p3">Item3</p>
+          <p class= "p" id="p1">Item1</p>
+          <p class= "p" id="p2">Item2</p>
+          <p class= "p" id="p3">Item3</p>
       </div>
-      <div class = "section1">
+      <div class= "section1" id="summary">
           <h2>DETAILED SUMMARY</h2>
           <p>
-              <br></br>
               sldmfhnldksjfhlkjdshfjkl<br></br>
               sdkjhfgbjdkshfgdkjhsfgkjh
               skjnhgbdfvbjkhdsgfjkhdsgfkjhdsgkjhfgdshjkfgjhkdsfgkjhdsfgkjhfgdehukjg<br></br>
