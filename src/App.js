9f3c9cb877e841a3e805cd05dc349f0ca98b0ae3
isaacmichael <54340385+isaacmichael@users.noreply.github.com>
2022-05-20 16:35:28 -0400

    added transitions for some items, added intor paragraph

--- src/App.js
+++ src/App.js
@@ -8,17 +8,21 @@ function App() {
       <div class="head">
           <h1 id="home">Isaac Pearson</h1>
           <h1 id="jobDescription">SOFTWARE DEVELOPER</h1>
+          <p id="REACT">MADE IN REACT</p>
+          <p id="AWS">HOSTED BY AWS</p>
+          <p id="DEPLOYMENT">USES CONTINUOUS DEPLOYMENT/TESTING</p>
 
 
       </div>
       <div class="main">
           <div class="introParagraph">
-              <p>sldmfhnldksjfhlkjdshfjkl
-                  sdkjhfgbjdkshfgdkjhsfgkjh
-                  skjnhgbdfvbjkhdsgfjkhdsgfkjhdsgkjhfgdshjkfgjhkdsfgkjhdsfgkjhfgdehukjg
-                  dl;ksfhjlkjdshfkljsdhkjdshklfhjklsdfh=
-                  dskjfhlkjdshflkdshlfkjhdsjklfhds
-                  dskjfhlkjdshflkdshlfkjhdsjklfhds</p>
+              <p>Hello, and welcome to my website! Here, I will showcase projects that I am currently working on as well as this website<br></br>
+                  Below, will be a mixture of school projects, agricultural analysis and other random things I find interesting.<br></br>
+                    <br></br>
+                  This website is stored in Github and deployed through AWS' Amplify service. Each commit triggers a series of events. <br></br>
+                  A test will be through Github's Actions for Node.js. After that, the product is sent to AWS and it automatically rebuilt and deployed.
+
+              </p>
           </div>
       <div class="grid">
           <h1>PORTFOLIO</h1>
