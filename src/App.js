129b306167782815d117b03080523de69bef65ca
isaacmichael <54340385+isaacmichael@users.noreply.github.com>
2022-05-20 18:50:02 -0400

    fixed responisveness of site

--- src/App.js
+++ src/App.js
@@ -50,9 +50,27 @@ function App() {
                    className="featured-img"></img>
               <p>Text Describing</p>
           </div>
+          <div className="item4">
+              <p>MySQL database</p>
+              <img src="https://static-assets.codecademy.com/Courses/Learn-CSS/Variables/Lesson/band1.jpg" alt=""
+                   className="featured-img"></img>
+              <p>Text Describing</p>
+          </div>
+          <div className="item5">
+              <p>Itunes API</p>
+              <img src="https://static-assets.codecademy.com/Courses/Learn-CSS/Variables/Lesson/band1.jpg" alt=""
+                   className="featured-img"></img>
+              <p>Text Describing</p>
+          </div>
+          <div className="item6">
+              <p>Machine Learning</p>
+              <img src="https://static-assets.codecademy.com/Courses/Learn-CSS/Variables/Lesson/band1.jpg" alt=""
+                   className="featured-img"></img>
+              <p>Text Describing</p>
+          </div>
       </div>
       <div class= "section1" id="summary">
-          <h2>DETAILED SUMMARY</h2>
+          <h2>ABOUT ME</h2>
           <p>
               sldmfhnldksjfhlkjdshfjkl
               sdkjhfgbjdkshfgdkjhsfgkjh
