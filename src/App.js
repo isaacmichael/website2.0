b41c703b7af4b8b63b1122f15b999b36ec28bbe3
isaacmichael <54340385+isaacmichael@users.noreply.github.com>
2022-05-19 22:38:51 -0400

    restart from scratch. Making portfolio page to highlight projects

--- src/App.js
+++ src/App.js
@@ -3,20 +3,35 @@ import './App.css';
 
 function App() {
   return (
+
       <body>
       <div class="head">
           <h1 id="home">Isaac Pearson</h1>
-          <h3>SOFTWARE DEVELOPER</h3>
-          <a class="nav">Sections</a>
-          <a href="#home" class="linkOne">1</a>
-          <a href="#summary" class="linkTwo">2</a>
-          <a href="#section3" class="linkThree">3</a>
+          <h1 id="jobDescription">SOFTWARE DEVELOPER</h1>
+
+
       </div>
       <div class="main">
       <div class="grid">
-          <p class= "p" id="p1">Drones In Agriculture</p>
-          <p class= "p" id="p2">Item2</p>
-          <p class= "p" id="p3">Item3</p>
+          <h1>PORTFOLIO</h1>
+          <div class="item1">
+            <p>Pirate Adventure</p>
+              <img src="https://static-assets.codecademy.com/Courses/Learn-CSS/Variables/Lesson/band1.jpg" alt=""
+                   className="featured-img"></img>
+              <p>Text Describing</p>
+          </div>
+          <div className="item2">
+              <p>Bug Tracker</p>
+              <img src="https://static-assets.codecademy.com/Courses/Learn-CSS/Variables/Lesson/band1.jpg" alt=""
+                   className="featured-img"></img>
+              <p>Text Describing</p>
+          </div>
+          <div className="item3">
+              <p>Lead Sigma Onboarding</p>
+              <img src="https://static-assets.codecademy.com/Courses/Learn-CSS/Variables/Lesson/band1.jpg" alt=""
+                   className="featured-img"></img>
+              <p>Text Describing</p>
+          </div>
       </div>
       <div class= "section1" id="summary">
           <h2>DETAILED SUMMARY</h2>
