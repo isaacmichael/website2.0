c28831aa48e7f1a6360cd26830c09965f8350609
isaacmichael <54340385+isaacmichael@users.noreply.github.com>
2022-05-27 00:05:03 -0400

    basic maintenance

--- src/App.js
+++ src/App.js
@@ -44,18 +44,28 @@ function App() {
             <p>Pirate Adventure</p>
               <img src="https://anthoncode.com/wp-content/uploads/2019/01/java-logo-png.png" alt="nothing" width="100" height="auto"
                    className="featured-img"></img>
-              <p>Created during the first semester in sophomore year with a team of three. Written in Java, JavaFX for UI</p>
+              <p>As a Sophomore, myself and a team of three others decided to build a game, despite the professor advising heavily against it. With JavaFX, we were able to create a 'Pirate Adventure Game'. The objective was to find the treasure and get
+                past a bunch of obstacles on the way. We integrated random events like giant squids, whirlpools, and other classy piratey things. A player would was able to traverse a map, and read an interesting story along the way while struggling to keep resources
+                  and crewmates from dissapearing.
+              </p>
               <img id="pirate2" src={pirate2} />
               <a href={'https://github.com/isaacmichael/Adventure-Game/tree/main/TeamEPirateAdventure2.0'} target={'_blank'}>
                   <button>Github Link</button>
               </a>
+              <p>
+                  By the end of the semester, it turned out much better than I had expected considering the groups experience level.
+              </p>
           </div>
 
           <div className="item2">
               <p>Bug Tracker</p>
               <img src="https://th.bing.com/th/id/R.95307d1659e1cb5122fe2d28b0d12ea8?rik=%2fPxCJe%2bFcVTfdg&riu=http%3a%2f%2fwww.sjcnet.co.uk%2fcontent%2fimages%2f2016%2f05%2fAspDotNet.png&ehk=P8aHZ9bIBsD%2fFRmwr%2f703h4YC6KiMAo9DZquYS%2fpzbY%3d&risl=&pid=ImgRaw&r=0" alt=""
                    className="featured-img"></img>
-              <p>Created a bug tracker with ASP.net web application. Uses Microsoft Database and encrypted all sensitive data</p>
+              <p>Created a bug tracker with the ASP.net web application. There were three types of users, Testers, Developers and Managers. Testers could submit bug tickets which would then be evaluated
+                by the manager. If the manager deemed the bug as an issue, it could be set to high, medium, or low severity levels. From there the Manager would send the bug ticket to a developer
+                  of their choosing. The developer can login and see the issue and once it is solved, the dev can submit a report on how it was solved and other factors. All passwords used in the database were encrypted.
+
+              </p>
               <a href={'https://github.com/isaacmichael/Web-Programming-Midterm/tree/master/MidtermProject'} target={'_blank'}>
                 <button>Github Link</button>
               </a>
@@ -72,28 +82,30 @@ function App() {
                   Implemented API into existing REACT UI to track usage of a given subscription for payment
               </p>
               <img id="leadSigma2" src={leadSigma}/>
-              <a href={''} target={'_blank'}>
-                  <button>Github Link</button>
-              </a>
+
           </div>
 
           <div className="item4">
               <p>Oracle Database</p>
               <img src="https://th.bing.com/th/id/R.f8b2bf7eb9f8a1ee124902bdc5da87ac?rik=jhQZ7841qr8Ywg&riu=http%3a%2f%2ferpinnews.com%2fwp-content%2fuploads%2f2017%2f10%2foracle-logo.png&ehk=U2x6i5WYcgGzSdSgE3UbWdJFgqaV9UyGA6xcg5F0Ffo%3d&risl=&pid=ImgRaw&r=0" alt=""
                    className="featured-img"></img>
-              <p>Created a database sophomore year to keep track of a factory's inventory</p>
+              <p>Created a database Sophomore year to keep track of a factory's inventory</p>
               <img id="sql1" src={SQL} width='500px'/>
-              <a href={''} target={'_blank'}>
-                  <button>Github Link</button>
-              </a>
+              <p>
+                  The database was made using Oracle's DBMS in a team of three.
+              </p>
+
           </div>
 
           <div className="item5">
               <p>Itunes API</p>
               <img src="https://th.bing.com/th/id/R.95307d1659e1cb5122fe2d28b0d12ea8?rik=%2fPxCJe%2bFcVTfdg&riu=http%3a%2f%2fwww.sjcnet.co.uk%2fcontent%2fimages%2f2016%2f05%2fAspDotNet.png&ehk=P8aHZ9bIBsD%2fFRmwr%2f703h4YC6KiMAo9DZquYS%2fpzbY%3d&risl=&pid=ImgRaw&r=0" alt=""
                    className="featured-img"></img>
-              <p>Created a Web application in ASP.net to search and retrieve data from Itunes' API</p>
-              <a href={''} target={'_blank'}>
+              <p>Created a Web application in ASP.net to search and retrieve data from Itunes' API
+                A user could enter any search term and modify the search category through a dropdown list. Categories are Movies, Music, or Software. From there all search results from the Itunes API would be displayed.
+
+              </p>
+              <a href={'https://github.com/isaacmichael/Itunes-search/tree/master/Project4'} target={'_blank'}>
                   <button>Github Link</button>
               </a>
           </div>
@@ -102,8 +114,9 @@ function App() {
               <p>Machine Learning</p>
               <img src="https://th.bing.com/th/id/R.9a8bd0fa820cc069f049829e093c016e?rik=NvH0xe7mTUlgvQ&riu=http%3a%2f%2fclipart-library.com%2fnew_gallery%2f289-2896071_python-logo-png-165709.png&ehk=aW7Pb2gc%2bysSZQp61jQfl77%2fq7MPVUC%2fFeEA%2bQt0R5I%3d&risl=&pid=ImgRaw&r=0" alt=""
                    className="featured-img"></img>
-              <p>Currently researching machine learning applications with jupiter notebook. Looking into tensor operations
-                  to speed up the calculation of large datasets.
+              <p>I am currently diving into machine learning applications with jupiter notebook. Looking into tensor operations
+                  to speed up the calculation of large datasets. I hope to gain a functional understanding of data Analysis and apply them to business and personal applications.
+                  My knowledge is surface level at the moment, but through Codecademy and my handy Kindle, I'm sure that I will make it far.
               </p>
               <a href={'https://github.com/isaacmichael/PythonMachineLearning'} target={'_blank'}>
                   <button>Github Link</button>
