8a71ec4178ae4bbcf839b81c79776463cc0bfe4c
isaacmichael <54340385+isaacmichael@users.noreply.github.com>
2022-05-27 21:08:19 -0400

    slight changes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import SQL from './photos/SQLDatabase.jpg'
 
 import leadSigma from './photos/leadSigma1.png'
 
+import dsm from './photos/dsm.png'
+import gli from './photos/gli.png'
+import ortho from './photos/ortho.png'
+
 
 function App() {
   return (
@@ -60,7 +64,7 @@ function App() {
           <div className="item2">
               <p>Bug Tracker</p>
               <img src="https://th.bing.com/th/id/R.95307d1659e1cb5122fe2d28b0d12ea8?rik=%2fPxCJe%2bFcVTfdg&riu=http%3a%2f%2fwww.sjcnet.co.uk%2fcontent%2fimages%2f2016%2f05%2fAspDotNet.png&ehk=P8aHZ9bIBsD%2fFRmwr%2f703h4YC6KiMAo9DZquYS%2fpzbY%3d&risl=&pid=ImgRaw&r=0" alt=""
-                   className="featured-img"></img>
+                   loading="lazy" className="featured-img"></img>
               <p>Created a bug tracker with the ASP.net web application. There were three types of users, Testers, Developers and Managers. Testers could submit bug tickets which would then be evaluated
                 by the manager. If the manager deemed the bug as an issue, it could be set to high, medium, or low severity levels. From there the Manager would send the bug ticket to a developer
                   of their choosing. The developer can login and see the issue and once it is solved, the dev can submit a report on how it was solved and other factors. All passwords used in the database were encrypted.
@@ -74,7 +78,7 @@ function App() {
           <div className="item3">
               <p>Lead Sigma Onboarding</p>
               <img id="leadSigma1" src="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco,dpr_1/i2z8uizotory1rtc3nay" alt=""
-                   className="featured-img"></img>
+                   loading="lazy" className="featured-img"></img>
               <p>
                   Helped create and maintain a microservice for Lead Sigma in 5 iterations using an Agile Methodology.
                   Worked with Ruby on Rails to create an API with a MYSQL database for tracking
@@ -88,7 +92,7 @@ function App() {
           <div className="item4">
               <p>Oracle Database</p>
               <img src="https://th.bing.com/th/id/R.f8b2bf7eb9f8a1ee124902bdc5da87ac?rik=jhQZ7841qr8Ywg&riu=http%3a%2f%2ferpinnews.com%2fwp-content%2fuploads%2f2017%2f10%2foracle-logo.png&ehk=U2x6i5WYcgGzSdSgE3UbWdJFgqaV9UyGA6xcg5F0Ffo%3d&risl=&pid=ImgRaw&r=0" alt=""
-                   className="featured-img"></img>
+                   loading="lazy" className="featured-img"></img>
               <p>Created a database Sophomore year to keep track of a factory's inventory</p>
               <img id="sql1" src={SQL} width='500px'/>
               <p>
@@ -100,7 +104,7 @@ function App() {
           <div className="item5">
               <p>Itunes API</p>
               <img src="https://th.bing.com/th/id/R.95307d1659e1cb5122fe2d28b0d12ea8?rik=%2fPxCJe%2bFcVTfdg&riu=http%3a%2f%2fwww.sjcnet.co.uk%2fcontent%2fimages%2f2016%2f05%2fAspDotNet.png&ehk=P8aHZ9bIBsD%2fFRmwr%2f703h4YC6KiMAo9DZquYS%2fpzbY%3d&risl=&pid=ImgRaw&r=0" alt=""
-                   className="featured-img"></img>
+                   loading="lazy" className="featured-img"></img>
               <p>Created a Web application in ASP.net to search and retrieve data from Itunes' API
                 A user could enter any search term and modify the search category through a dropdown list. Categories are Movies, Music, or Software. From there all search results from the Itunes API would be displayed.
 
@@ -113,7 +117,33 @@ function App() {
           <div className="item6">
               <p>Machine Learning</p>
               <img src="https://th.bing.com/th/id/R.9a8bd0fa820cc069f049829e093c016e?rik=NvH0xe7mTUlgvQ&riu=http%3a%2f%2fclipart-library.com%2fnew_gallery%2f289-2896071_python-logo-png-165709.png&ehk=aW7Pb2gc%2bysSZQp61jQfl77%2fq7MPVUC%2fFeEA%2bQt0R5I%3d&risl=&pid=ImgRaw&r=0" alt=""
-                   className="featured-img"></img>
+                   loading="lazy" className="featured-img"></img>
+              <p>I am currently diving into machine learning applications with jupiter notebook. Looking into tensor operations
+                  to speed up the calculation of large datasets. I hope to gain a functional understanding of data Analysis and apply them to business and personal applications.
+                  My knowledge is surface level at the moment, but through Codecademy and my handy Kindle, I'm sure that I will make it far.
+              </p>
+              <a href={'https://github.com/isaacmichael/PythonMachineLearning'} target={'_blank'}>
+                  <button>Github Link</button>
+              </a>
+          </div>
+
+          <div className="item7">
+              <p>Drone Photogrammetry</p>
+              <img src="https://th.bing.com/th/id/R.9a8bd0fa820cc069f049829e093c016e?rik=NvH0xe7mTUlgvQ&riu=http%3a%2f%2fclipart-library.com%2fnew_gallery%2f289-2896071_python-logo-png-165709.png&ehk=aW7Pb2gc%2bysSZQp61jQfl77%2fq7MPVUC%2fFeEA%2bQt0R5I%3d&risl=&pid=ImgRaw&r=0" alt=""
+                   loading="lazy" className="featured-img"></img>
+              <p>I am currently diving into machine learning applications with jupiter notebook. Looking into tensor operations
+                  to speed up the calculation of large datasets. I hope to gain a functional understanding of data Analysis and apply them to business and personal applications.
+                  My knowledge is surface level at the moment, but through Codecademy and my handy Kindle, I'm sure that I will make it far.
+              </p>
+              <a href={'https://github.com/isaacmichael/PythonMachineLearning'} target={'_blank'}>
+                  <button>Github Link</button>
+              </a>
+          </div>
+
+          <div className="item8">
+              <p>CNC</p>
+              <img src="https://th.bing.com/th/id/R.9a8bd0fa820cc069f049829e093c016e?rik=NvH0xe7mTUlgvQ&riu=http%3a%2f%2fclipart-library.com%2fnew_gallery%2f289-2896071_python-logo-png-165709.png&ehk=aW7Pb2gc%2bysSZQp61jQfl77%2fq7MPVUC%2fFeEA%2bQt0R5I%3d&risl=&pid=ImgRaw&r=0" alt=""
+                   loading="lazy" className="featured-img"></img>
               <p>I am currently diving into machine learning applications with jupiter notebook. Looking into tensor operations
                   to speed up the calculation of large datasets. I hope to gain a functional understanding of data Analysis and apply them to business and personal applications.
                   My knowledge is surface level at the moment, but through Codecademy and my handy Kindle, I'm sure that I will make it far.
@@ -133,8 +163,6 @@ function App() {
               software developer alongside completing courses at Codecademy to sharpen my skills.<br></br>
               Aside from programming, I operate a consumer grade CNC, 3D model and print, and create high quality orthomosaics (maps) to better show
               areas of high and low elevation as well as differences in vegetation within farmland.
-
-
           </p>
       </div>
 
