443be5b848092e6739d51731a142524fbbce8be7
isaacmichael <54340385+isaacmichael@users.noreply.github.com>
2022-05-31 00:19:41 -0400

    updated responsive layout

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App() {
                    class="featured-img"></img>
               <p>As a Sophomore, myself and a team of three others decided to build a game, despite the professor advising heavily against it. With JavaFX, we were able to create a 'Pirate Adventure Game'. The objective was to find the treasure and get
                 past a bunch of obstacles on the way. We integrated random events like giant squids, whirlpools, and other classy piratey things. A player would was able to traverse a map, and read an interesting story along the way while struggling to keep resources
-                  and crewmates from dissapearing.
+                  and crewmates from disappearing.
               </p>
               <img id="pic" src={pirate2} />
               <p></p>
@@ -66,10 +66,10 @@ function App() {
               <h2>Bug Tracker</h2>
               <img width="200" src="https://th.bing.com/th/id/R.95307d1659e1cb5122fe2d28b0d12ea8?rik=%2fPxCJe%2bFcVTfdg&riu=http%3a%2f%2fwww.sjcnet.co.uk%2fcontent%2fimages%2f2016%2f05%2fAspDotNet.png&ehk=P8aHZ9bIBsD%2fFRmwr%2f703h4YC6KiMAo9DZquYS%2fpzbY%3d&risl=&pid=ImgRaw&r=0" alt=""
                    loading="lazy" class="featured-img"></img>
-              <p>Created a bug tracker with the ASP.net web application. There were three types of users, Testers, Developers and Managers. Testers could submit bug tickets which would then be evaluated
+              <p>
+                  Created a bug tracker with the ASP.net web application. There were three types of users, Testers, Developers and Managers. Testers could submit bug tickets which would then be evaluated
                 by the manager. If the manager deemed the bug as an issue, it could be set to high, medium, or low severity levels. From there the Manager would send the bug ticket to a developer
                   of their choosing. The developer can login and see the issue and once it is solved, the dev can submit a report on how it was solved and other factors. All passwords used in the database were encrypted.
-
               </p>
               <a href={'https://github.com/isaacmichael/Web-Programming-Midterm/tree/master/MidtermProject'} target={'_blank'}>
                 <button>Github Link</button>
@@ -81,12 +81,22 @@ function App() {
               <img id="leadSigma1" width="100" src="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco,dpr_1/i2z8uizotory1rtc3nay" alt=""
                    loading="lazy" class="featured-img"></img>
               <p>
-                  Helped create and maintain a microservice for Lead Sigma in 5 iterations using an Agile Methodology.
+                  During a two semester project, I
+                  helped create and maintain a microservice for Lead Sigma in 5 iterations using an Agile Methodology.
                   Worked with Ruby on Rails to create an API with a MYSQL database for tracking
                   subscriptions and their features uses (calls, texts, emails).
                   Implemented API into existing REACT UI to track usage of a given subscription for payment
               </p>
               <img id="pic" src={leadSigma}/>
+              <p>
+                  The 'Subscription Service' on the right is the API microservice that the group
+                  created. It holds info such as features used, subscription type, member count
+                  and and more. <br></br>
+                  This service communicated with the 'Task List UI' and presented the information
+                  in a clear and concise way to the user. The user could view their current usage
+                  and the corresponding cost.
+
+              </p>
 
           </div>
 
@@ -94,10 +104,11 @@ function App() {
               <h2>Oracle Database</h2>
               <img width="200" src="https://th.bing.com/th/id/R.f8b2bf7eb9f8a1ee124902bdc5da87ac?rik=jhQZ7841qr8Ywg&riu=http%3a%2f%2ferpinnews.com%2fwp-content%2fuploads%2f2017%2f10%2foracle-logo.png&ehk=U2x6i5WYcgGzSdSgE3UbWdJFgqaV9UyGA6xcg5F0Ffo%3d&risl=&pid=ImgRaw&r=0" alt=""
                    loading="lazy" className="featured-img"></img>
-              <p>Created a database Sophomore year to keep track of a factory's inventory</p>
+              <p>Created a database Sophomore year to keep track of a mock factory's inventory.
+              </p>
               <img id="sql1" src={SQL} width='500px'/>
               <p>
-                  The database was made using Oracle's DBMS in a team of three.
+                  The database was made using Oracle's DBMS in a team of three over the course of a semester.
               </p>
 
           </div>
@@ -121,7 +132,7 @@ function App() {
                    loading="lazy" className="featured-img"></img>
               <p>I am currently diving into machine learning applications with jupiter notebook. Looking into tensor operations
                   to speed up the calculation of large datasets. I hope to gain a functional understanding of data Analysis and apply them to business and personal applications.
-                  My knowledge is surface level at the moment, but through Codecademy and my handy Kindle, I'm sure that I will make it far.
+                  My knowledge is surface level at the moment, but with Codecademy and my handy Kindle, I'm sure that I'll make it far.
               </p>
               <a href={'https://github.com/isaacmichael/PythonMachineLearning'} target={'_blank'}>
                   <button>Github Link</button>
@@ -136,25 +147,25 @@ function App() {
                   This model took about ~700 photos to get important elevation maps and GLI maps.
               </p>
               <img id="pic" src={gli} alt="" loading="lazy" className="featured-img"></img>
-              <p></p>
+              <p>GLI (green leaf index)</p>
               <img id="pic" src={dsm} alt="" loading="lazy" className="featured-img"></img>
-              <p></p>
+              <p>DSM (digital surface model</p>
               <iframe width="500" height="300" src="https://www.youtube.com/embed/5hgq2SEYpa0"
                       title="YouTube video player" frameBorder="0"
                       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                       allowFullScreen>
               </iframe>
               <p>
-                  A model of Best-One Tire upland. Taken With a Phantom 4 Pro Drone
+                  A model of Best-One Tire Upland. Taken With a Phantom 4 Pro Drone
                   Crosshatch survey consisting of ~500 photos for better 3D imaging.
-                  Mainly, I created this map for fun, but I also find it important to preserve the state of a given location with something
-                  more substantial than a photo. If a picture is worth a thousand words, I wonder what a 3D model is worth?
+                  Mainly, I created this map for fun, but I also find it important as well as interesting to preserve
+                  locations with 3D models instead of individual pictures. I often print out my 3D models
+                  as well.
               </p>
               <iframe title="odm_textured_model_geo" frameBorder="0" allowFullScreen mozallowfullscreen="true"
                       webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking
                       execution-while-out-of-viewport execution-while-not-rendered web-share
                       src="https://sketchfab.com/models/4bcc4fc75afa4e35827e3910e035100e/embed">
-
               </iframe>
           </div>
 
